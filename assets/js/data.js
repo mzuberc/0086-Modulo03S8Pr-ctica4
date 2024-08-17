43f9db6519847e8b16161a14e0540f117c6b3510
Michelle Zuber <mzuberc@gmail.com>
2024-08-17 11:52:13 +1000

    Práctica Listado SuperHéroes

---
+++ assets/js/data.js
@@ -0,0 +1,581 @@
+var superHeroes = [
+    {
+        "response": "success",
+        "id": "1",
+        "name": "A-Bomb",
+        "powerstats": {
+            "intelligence": "38",
+            "strength": "100",
+            "speed": "17",
+            "durability": "80",
+            "power": "24",
+            "combat": "64"
+        },
+        "biography": {
+            "full-name": "Richard Milhouse Jones",
+            "alter-egos": "No alter egos found.",
+            "aliases": [
+                "Rick Jones"
+            ],
+            "place-of-birth": "Scarsdale, Arizona",
+            "first-appearance": "Hulk Vol 2 #2 (April, 2008) (as A-Bomb)",
+            "publisher": "Marvel Comics",
+            "alignment": "good"
+        },
+        "appearance": {
+            "gender": "Male",
+            "race": "Human",
+            "height": [
+                "6'8",
+                "203 cm"
+            ],
+            "weight": [
+                "980 lb",
+                "441 kg"
+            ],
+            "eye-color": "Yellow",
+            "hair-color": "No Hair"
+        },
+        "work": {
+            "occupation": "Musician, adventurer, author; formerly talk show host",
+            "base": "-"
+        },
+        "connections": {
+            "group-affiliation": "Hulk Family; Excelsior (sponsor), Avengers (honorary member); formerly partner of the Hulk, Captain America and Captain Marvel; Teen Brigade; ally of Rom",
+            "relatives": "Marlo Chandler-Jones (wife); Polly (aunt); Mrs. Chandler (mother-in-law); Keith Chandler, Ray Chandler, three unidentified others (brothers-in-law); unidentified father (deceased); Jackie Shorr (alleged mother; unconfirmed)"
+        },
+        "image": {
+            "url": "https://www.superherodb.com/pictures2/portraits/10/100/10060.jpg"
+        }
+    },
+    {
+        "response": "success",
+        "id": "10",
+        "name": "Agent Bob",
+        "powerstats": {
+            "intelligence": "10",
+            "strength": "8",
+            "speed": "13",
+            "durability": "5",
+            "power": "5",
+            "combat": "20"
+        },
+        "biography": {
+            "full-name": "Bob",
+            "alter-egos": "No alter egos found.",
+            "aliases": [
+                "Bob",
+                "agent of Hydra",
+                "Bob",
+                "agent of A.I.M"
+            ],
+            "place-of-birth": "-",
+            "first-appearance": "Cable & Deadpool #38 (May, 2007)",
+            "publisher": "Marvel Comics",
+            "alignment": "good"
+        },
+        "appearance": {
+            "gender": "Male",
+            "race": "Human",
+            "height": [
+                "5'10",
+                "178 cm"
+            ],
+            "weight": [
+                "181 lb",
+                "81 kg"
+            ],
+            "eye-color": "Brown",
+            "hair-color": "Brown"
+        },
+        "work": {
+            "occupation": "Mercenary, janitor; former pirate, terrorist",
+            "base": "Mobile; formerly Manhattan, Hellcarrier"
+        },
+        "connections": {
+            "group-affiliation": "A.I.M., Deadpool; formerly Agency X, Hydra",
+            "relatives": "Allison (ex-wife); Terry and Howie (sons)"
+        },
+        "image": {
+            "url": "https://www.superherodb.com/pictures2/portraits/10/100/10255.jpg"
+        }
+    },
+    {
+        "response": "success",
+        "id": "15",
+        "name": "Alex Mercer",
+        "powerstats": {
+            "intelligence": "50",
+            "strength": "80",
+            "speed": "42",
+            "durability": "90",
+            "power": "100",
+            "combat": "50"
+        },
+        "biography": {
+            "full-name": "Alexander J. Mercer",
+            "alter-egos": "No alter egos found.",
+            "aliases": [
+                "Prototype",
+                "Zues",
+                "Blacklight"
+            ],
+            "place-of-birth": "-",
+            "first-appearance": "-",
+            "publisher": "Wildstorm",
+            "alignment": "bad"
+        },
+        "appearance": {
+            "gender": "Male",
+            "race": "Human",
+            "height": [
+                "-",
+                "0 cm"
+            ],
+            "weight": [
+                "- lb",
+                "0 kg"
+            ],
+            "eye-color": "-",
+            "hair-color": "-"
+        },
+        "work": {
+            "occupation": "-",
+            "base": "-"
+        },
+        "connections": {
+            "group-affiliation": "Gentek, The Infected",
+            "relatives": "Dana Mercer (sister)"
+        },
+        "image": {
+            "url": "https://www.superherodb.com/pictures2/portraits/10/100/10398.jpg"
+        }
+    },
+    {
+        "response": "success",
+        "id": "20",
+        "name": "Amazo",
+        "powerstats": {
+            "intelligence": "63",
+            "strength": "100",
+            "speed": "83",
+            "durability": "100",
+            "power": "100",
+            "combat": "100"
+        },
+        "biography": {
+            "full-name": "",
+            "alter-egos": "No alter egos found.",
+            "aliases": [
+                "Professor Ivos Amazing Android",
+                "Timazo",
+                "Humazo",
+                "Hourmazo"
+            ],
+            "place-of-birth": "-",
+            "first-appearance": "Brave and the Bold #30 (July, 1960)",
+            "publisher": "DC Comics",
+            "alignment": "bad"
+        },
+        "appearance": {
+            "gender": "Male",
+            "race": "Android",
+            "height": [
+                "8'5",
+                "257 cm"
+            ],
+            "weight": [
+                "385 lb",
+                "173 kg"
+            ],
+            "eye-color": "Red",
+            "hair-color": "-"
+        },
+        "work": {
+            "occupation": "-",
+            "base": "-"
+        },
+        "connections": {
+            "group-affiliation": "Formerly the Secret Society of Super Villains",
+            "relatives": "Professor Ivo (creator), Kid Amazo (cyborg offspring)"
+        },
+        "image": {
+            "url": "https://www.superherodb.com/pictures2/portraits/10/100/1390.jpg"
+        }
+    },
+    {
+        "response": "success",
+        "id": "25",
+        "name": "Angel Dust",
+        "powerstats": {
+            "intelligence": "38",
+            "strength": "55",
+            "speed": "23",
+            "durability": "42",
+            "power": "17",
+            "combat": "30"
+        },
+        "biography": {
+            "full-name": "Christina",
+            "alter-egos": "No alter egos found.",
+            "aliases": [
+                "Angel",
+                "Dusty"
+            ],
+            "place-of-birth": "-",
+            "first-appearance": "Morlocks #1",
+            "publisher": "Marvel Comics",
+            "alignment": "good"
+        },
+        "appearance": {
+            "gender": "Female",
+            "race": "Mutant",
+            "height": [
+                "5'5",
+                "165 cm"
+            ],
+            "weight": [
+                "126 lb",
+                "57 kg"
+            ],
+            "eye-color": "Yellow",
+            "hair-color": "Black"
+        },
+        "work": {
+            "occupation": "-",
+            "base": "Chicago, Illinois"
+        },
+        "connections": {
+            "group-affiliation": "-",
+            "relatives": "-"
+        },
+        "image": {
+            "url": "https://www.superherodb.com/pictures2/portraits/10/100/10405.jpg"
+        }
+    },
+    {
+        "response": "success",
+        "id": "30",
+        "name": "Ant-Man",
+        "powerstats": {
+            "intelligence": "100",
+            "strength": "18",
+            "speed": "23",
+            "durability": "28",
+            "power": "32",
+            "combat": "32"
+        },
+        "biography": {
+            "full-name": "Hank Pym",
+            "alter-egos": "Giant-Man, Goliath, Wasp II, Yellowjacket",
+            "aliases": [
+                "Hank Pym",
+                "Doctor Pym",
+                "Ant-Man",
+                "Goliath",
+                "Yellowjacket",
+                "Wasp",
+                "Earth's Scientist Supreme"
+            ],
+            "place-of-birth": "Elmsford, New York",
+            "first-appearance": "Tales to Astonish #27 (January, 1962) (as Hank Pym)  Tales to Astonish #35 (September, 1962) (as Ant-Man)",
+            "publisher": "Giant-Man",
+            "alignment": "good"
+        },
+        "appearance": {
+            "gender": "Male",
+            "race": "Human",
+            "height": [
+                "6'11",
+                "211 cm"
+            ],
+            "weight": [
+                "270 lb",
+                "122 kg"
+            ],
+            "eye-color": "Blue",
+            "hair-color": "Blond"
+        },
+        "work": {
+            "occupation": "Adventurer, Biochemist, former manager of Avengers Compound",
+            "base": "Avengers Compound, Los Angeles; formerly Infinite Avengers Mansion; Captive aboard a Skrull ship; Avengers Mansion, New York City, New York"
+        },
+        "connections": {
+            "group-affiliation": "Avengers Academy, Secret Avengers; formerly Mighty Avengers, Avengers (founding member), Defenders, Future Iron Man's Team",
+            "relatives": "-"
+        },
+        "image": {
+            "url": "https://www.superherodb.com/pictures2/portraits/10/100/857.jpg"
+        }
+    },
+    {
+        "response": "success",
+        "id": "35",
+        "name": "Apocalypse",
+        "powerstats": {
+            "intelligence": "100",
+            "strength": "100",
+            "speed": "33",
+            "durability": "100",
+            "power": "100",
+            "combat": "60"
+        },
+        "biography": {
+            "full-name": "En Sabah Nur",
+            "alter-egos": "No alter egos found.",
+            "aliases": [
+                "The Eternal One",
+                "the High Lord",
+                "Set",
+                "Huitxilopochti",
+                "Sauru",
+                "Kali-Ma"
+            ],
+            "place-of-birth": "Akkaba, Egypt",
+            "first-appearance": "X-Factor #5 (June, 1986)",
+            "publisher": "Marvel Comics",
+            "alignment": "bad"
+        },
+        "appearance": {
+            "gender": "Male",
+            "race": "Mutant",
+            "height": [
+                "7'0",
+                "213 cm"
+            ],
+            "weight": [
+                "300 lb",
+                "135 kg"
+            ],
+            "eye-color": "Red",
+            "hair-color": "Black"
+        },
+        "work": {
+            "occupation": "Student; formerly Conqueror; Scientist",
+            "base": "Celestial Ship, mobile"
+        },
+        "connections": {
+            "group-affiliation": "Clan Akkaba, employer of Apocalypse's Horsemen, Dark Riders, former employer of the Alliance of Evil, 198",
+            "relatives": "Baal of the Crimson Sands (adopted father, deceased)"
+        },
+        "image": {
+            "url": "https://www.superherodb.com/pictures2/portraits/10/100/852.jpg"
+        }
+    },
+    {
+        "response": "success",
+        "id": "40",
+        "name": "Archangel",
+        "powerstats": {
+            "intelligence": "63",
+            "strength": "13",
+            "speed": "58",
+            "durability": "64",
+            "power": "35",
+            "combat": "42"
+        },
+        "biography": {
+            "full-name": "Warren Kenneth Worthington III",
+            "alter-egos": "Angel",
+            "aliases": [
+                "War",
+                "Dark Angel",
+                "Death",
+                "Avenging Angel"
+            ],
+            "place-of-birth": "Centerport, Long Island, New York",
+            "first-appearance": "X-Men #1 (September, 1963)",
+            "publisher": "Angel",
+            "alignment": "good"
+        },
+        "appearance": {
+            "gender": "Male",
+            "race": "Mutant",
+            "height": [
+                "6'0",
+                "183 cm"
+            ],
+            "weight": [
+                "150 lb",
+                "68 kg"
+            ],
+            "eye-color": "Blue",
+            "hair-color": "Blond"
+        },
+        "work": {
+            "occupation": "Adventurer, Chairman & principal stockholder of Worthington Industries, Multi-Millionaire, Heir, former terrorist",
+            "base": "-"
+        },
+        "connections": {
+            "group-affiliation": "X-Men (founding member); X-Force; Mutantes Sans Fronti�res; Hellfire Club; formerly X-Club (founding member); Death's champions; Secret Defenders; X-Factor/X-Terminators (founding member); Horsemen of Apocalypse; Defenders; Champions of Los Angeles (fou",
+            "relatives": "Wallace Worthington (paternal ancestor, deceased), Warren Worthington Sr. (paternal grandfather, deceased), Warren Worthington Jr. (father, deceased), Kathryn Worthington (mother, deceased), Burt Worthington (paternal uncle), Mimi (maternal aunt), unnamed distant cousin"
+        },
+        "image": {
+            "url": "https://www.superherodb.com/pictures2/portraits/10/100/838.jpg"
+        }
+    },
+    {
+        "response": "success",
+        "id": "45",
+        "name": "Armor",
+        "powerstats": {
+            "intelligence": "50",
+            "strength": "63",
+            "speed": "12",
+            "durability": "80",
+            "power": "55",
+            "combat": "54"
+        },
+        "biography": {
+            "full-name": "Hisako Ichiki",
+            "alter-egos": "No alter egos found.",
+            "aliases": [
+                "X-Girl",
+                "Kid"
+            ],
+            "place-of-birth": "-",
+            "first-appearance": "Astonishing X-Men #4 (2004)",
+            "publisher": "Marvel Comics",
+            "alignment": "good"
+        },
+        "appearance": {
+            "gender": "Female",
+            "race": "null",
+            "height": [
+                "5'4",
+                "163 cm"
+            ],
+            "weight": [
+                "112 lb",
+                "50 kg"
+            ],
+            "eye-color": "Black",
+            "hair-color": "Black"
+        },
+        "work": {
+            "occupation": "Adventurer, student",
+            "base": "-"
+        },
+        "connections": {
+            "group-affiliation": "X-Men, formerly: Xavier Institute Student Body, New X-Men",
+            "relatives": "-"
+        },
+        "image": {
+            "url": "https://www.superherodb.com/pictures2/portraits/10/100/1347.jpg"
+        }
+    },
+    {
+        "response": "success",
+        "id": "599",
+        "name": "Silverclaw",
+        "powerstats": {
+            "intelligence": "50",
+            "strength": "28",
+            "speed": "35",
+            "durability": "42",
+            "power": "34",
+            "combat": "42"
+        },
+        "biography": {
+            "full-name": "Maria de Guadalupe Santiago",
+            "alter-egos": "No alter egos found.",
+            "aliases": [
+                "La Garra Argentado",
+                "Daughter of the Volcano God,"
+            ],
+            "place-of-birth": "The village of Kamekeri, Costa Verde",
+            "first-appearance": "Avengers #8 (1998)",
+            "publisher": "Marvel Comics",
+            "alignment": "good"
+        },
+        "appearance": {
+            "gender": "Female",
+            "race": "null",
+            "height": [
+                "5'2",
+                "157 cm"
+            ],
+            "weight": [
+                "110 lb",
+                "50 kg"
+            ],
+            "eye-color": "Brown",
+            "hair-color": "Black"
+        },
+        "work": {
+            "occupation": "University student, adventurer",
+            "base": "Empire State University and Avengers Mansion, New York City, NY"
+        },
+        "connections": {
+            "group-affiliation": "The Secret Avengers, formerly reserve member of the Avengers",
+            "relatives": "Jaime Santiago (father), Peliali (mother), Edwin Jarvis (\"uncle\")"
+        },
+        "image": {
+            "url": "https://www.superherodb.com/pictures2/portraits/10/100/1019.jpg"
+        }
+    },
+    {
+        "response": "success",
+        "id": "620",
+        "name": "Spider-Man",
+        "powerstats": {
+            "intelligence": "90",
+            "strength": "55",
+            "speed": "67",
+            "durability": "75",
+            "power": "74",
+            "combat": "85"
+        },
+        "biography": {
+            "full-name": "Peter Parker",
+            "alter-egos": "No alter egos found.",
+            "aliases": [
+                "Spiderman",
+                "Bag-Man",
+                "Black Marvel",
+                "Captain Universe",
+                "Dusk",
+                "Green Hood",
+                "Hornet",
+                "Mad Dog 336",
+                "Peter Palmer",
+                "Prodigy",
+                "Ricochet",
+                "Scarlet Spider",
+                "Spider-Boy",
+                "Spider-Hulk",
+                "Spider-Morphosis"
+            ],
+            "place-of-birth": "New York, New York",
+            "first-appearance": "Amazing Fantasy #15",
+            "publisher": "Marvel Comics",
+            "alignment": "good"
+        },
+        "appearance": {
+            "gender": "Male",
+            "race": "Human",
+            "height": [
+                "5'10",
+                "178 cm"
+            ],
+            "weight": [
+                "165 lb",
+                "74 kg"
+            ],
+            "eye-color": "Hazel",
+            "hair-color": "Brown"
+        },
+        "work": {
+            "occupation": "Freelance photographer, teacher",
+            "base": "New York, New York"
+        },
+        "connections": {
+            "group-affiliation": "Member of the Avengers, formerly member of Outlaws, alternate Fantastic Four",
+            "relatives": "Richard Parker (father, deceased), Mary Parker(mother, deceased), Benjamin Parker (uncle, deceased), May Parker (aunt), Mary Jane Watson-Parker (wife), May Parker (daughter, allegedly deceased)"
+        },
+        "image": {
+            "url": "https://www.superherodb.com/pictures2/portraits/10/100/133.jpg"
+        }
+    }
+
+];
---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+console.log(superHeroes);
+
+
+function listarSuperHeroes(listado) {
+    $("#listado-super-heroes tbody").html("");
+    for (const item of listado) {
+        $("#listado-super-heroes tbody").append(`
+            <tr>
+                <td>${item.id}</td>
+                <td>${item.name}</td>
+                <td>${item.work.occupation}</td>
+                <td>* ${item.biography.aliases.join("<br>* ")}</td>
+                <td>
+                    <img class="img-avatar" src="${item.image.url}">
+                </td>
+                <td>
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                            Acciones
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item link-eliminar" data-id="${item.id}" href="#">Eliminar</a></li>
+                        </ul>
+                    </div>
+                </td>
+            </tr>
+        `);
+
+    }
+
+}
+
+listarSuperHeroes(superHeroes)
+
+$(function() {
+    $(document).on("click", ".link-eliminar", function(event){
+        event.preventDefault();
+        var idEliminar = $(this).data("id")
+
+        if(confirm("¿Seguro desea eliminar este super héroe?")) {
+            //Ejemplo 1 para eliminar
+            // var indice = superHeroes.findIndex(item => item.id == idEliminar)
+            // superHeroes.splice(indice, 1)
+
+            //Ejemplo 2 para eliminar
+            superHeroes = superHeroes.filter(item => item.id != idEliminar)
+
+            listarSuperHeroes(superHeroes)
+        }
+    })
+
+    $("#mostrar-ocultar").click(function() {
+        if($("#filtros").hasClass("d-none")) { // Lógica para mostrar
+            $("#filtros").removeClass("d-none")
+            $("#mostrar-ocultar").removeClass("btn-success").addClass("btn-danger").html("Ocultar")
+        } else { // Lógica para ocultar
+            $("#filtros").addClass("d-none")
+            $("#mostrar-ocultar").removeClass("btn-danger").addClass("btn-success").html("Mostrar")
+        }
+    })
+
+})
+
+
